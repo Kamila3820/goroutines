dcab55cfcac900204225b1232399c58eea9ee930
Thanadol Saojarkaval <98137663+Kamila3820@users.noreply.github.com>
2025-01-12 14:35:12 +0700

    - going: select with goroutine

---
+++ selectroutine/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+	channel1 := make(chan int)
+	channel2 := make(chan int)
+
+	go func() {
+		channel1 <- 10
+		close(channel1)
+	}()
+	go func() {
+		channel2 <- 20
+		close(channel2) // channel after closed, it handle itself to be zero value
+	}()
+
+	closeChannel1, closeChannel2 := false, false
+
+	for {
+		if closeChannel1 && closeChannel2 {
+			break
+		}
+		select {
+		case v, ok := <-channel1:
+			if !ok {
+				closeChannel1 = true
+				continue
+			}
+			fmt.Println(v)
+		case v, ok := <-channel2:
+			if !ok {
+				closeChannel2 = true
+				continue
+			}
+			fmt.Println(v)
+		default:
+			fmt.Println("No message received")
+		}
+
+	}
+}
